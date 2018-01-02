11999d4a9fc1dad152e81a9f6fd4a436713cec4f
Andrea McKenzie <mckenzie.andrea.m@gmail.com>
2018-01-02 15:35:46 -0500

    server is up, working on the index.html

--- server.js
+++ server.js
@@ -1,17 +1,48 @@
+// EXPRESS DEPENDANCIES
 const express = require('express');
-const socket = require('socket.io');
 const logger = require('morgan');
 const parser = require('body-parser');
 
 const app = express();
 
-app.use(logger('dev'))
+// SOCKET DEPENDENCIES
+const ioServer = require('http').createServer(app)
+const socket = require('socket.io')(ioServer);
 
+const usernames = [];
+
+// EXPRESS MIDDLEWARE
+app.use(logger('dev'))
 app.use(parser.json())
 app.use(parser.urlencoded({extended: false}))
 
-app.listen(3000);
 
+socket.on('connection',function(connection){
+  console.log('new connection from: ' + connection.id);
+  usernames.push({username: "", connection });
+  console.log('You have ' + usernames.length + ' users connected.')
+
+  connection.on('send', function(e){
+    console.log(e + 'just happened!')
+    connection.emit('message', e)
+  })
+
+  connection.on('typing', function(e){
+    connection.broadcast.emit('typing', e)
+  })
+
+  connection.on('disconnect', function(e){
+    console.log(connection.id + " disconnected.")
+    let index = usernames.indexOf(connection.id)
+    usernames.splice(index, 1);
+    console.log(usernames.length + ' users connected.')
+  })
+})
+
+
+
+// APP PORTS AND ROUTING
+ioServer.listen(3000);
 app.get("/", (req,res,next)=>{
   res.send('hi')
 })
